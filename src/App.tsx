88de3003824d10c2679c14725ae0ff3353328363
Ajyamkar <ajyamkar99@gmail.com>
2023-07-02 02:03:23 +0530

    Added routes and their respective components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import AddExpensesDrawer from "./components/AddExpensesDrawer/AddExpensesDrawer";
+import Analytics from "./components/Analytics/Analytics";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Expenses from "./components/Expenses/Expenses";
+import Login from "./components/Login/Login";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Profile from "./components/Profile/Profile";
+import Signup from "./components/Signup/Signup";
 import Navbar from "./components/TopAppBar/Navbar";
 
 function App() {
   return (
     <div className="app-container">
-      <Navbar />
-      <AddExpensesDrawer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
+  );
+}
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <AddExpensesDrawer />
+    </>
   );
 }
 
