4522a1bf29b660151dc812af700d6f433592a4d5
Ajyamkar <ajyamkar99@gmail.com>
2023-09-16 21:59:35 +0530

    added an api call to get user income for selected month

--- src/api/income.ts
+++ src/api/income.ts
@@ -17,6 +17,10 @@ const getIncomeCategoriesApi = async () => {
   return response;
 };
 
+/**
+ * Api call to save user income
+ * @param data - income related data to save eg: amount, category
+ */
 const saveUserIncome = async (data: addIncomeDataType) => {
   const response = await instance({
     url: "/api/income/addNewIncome",
@@ -29,4 +33,22 @@ const saveUserIncome = async (data: addIncomeDataType) => {
   return response;
 };
 
-export { getIncomeCategoriesApi, saveUserIncome };
+/**
+ * Api call to get user income for selected month
+ * @param selectedDate - selected date
+ */
+const getUserIncome = async (selectedDate: Date) => {
+  const month = new Date(selectedDate).toLocaleString("default", {
+    month: "long",
+  });
+  const response = instance({
+    method: "GET",
+    url: `/api/income/getIncome/${month}`,
+    headers: {
+      authorization: getUserAuthorizationToken(),
+    },
+  });
+  return response;
+};
+
+export { getIncomeCategoriesApi, saveUserIncome, getUserIncome };
