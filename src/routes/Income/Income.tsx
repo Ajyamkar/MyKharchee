435d114ddf16fb530af4070539b51e9a1514acad
Ajyamkar <ajyamkar99@gmail.com>
2023-09-18 00:31:27 +0530

    Consumed getUserIncome api

--- src/routes/Income/Income.tsx
+++ src/routes/Income/Income.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { getUserIncome } from "../../api/income";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const Income = () => {
+  React.useEffect(() => {
+    getUserIncome(new Date())
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Income</h1>
+      <div>
+        <h1>Income</h1>
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DatePicker"]}>
+            <DatePicker
+              label={'"month" and "year"'}
+              views={["month", "year"]}
+              onChange={(newValue) => {
+                console.log(newValue);
+              }}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+      </div>
       <Outlet />
     </div>
   );
